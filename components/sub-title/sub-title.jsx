dc955d5354155fd866033c18bd05827532e074b3
jidan <wildanavi2@gmail.com>
2024-10-21 22:47:30 +0700

    feat(component): add sub-title

---
+++ components/sub-title/sub-title.jsx
@@ -0,0 +1,11 @@
+function SubTitle() {
+	return (
+		<div className='bg-[#1F3972] py-2'>
+			<div className='text-xs text-white'>This video is confidential & contains</div>
+			<div className='text-xs text-white'>proprietary information & intellectual property of</div>
+			<div className='text-xs font-bold text-white'>PT. INGENIO Medika Nusantara</div>
+		</div>
+	);
+}
+
+export {SubTitle};
